24364960c0dac56a73c5fe865ec9b62a8fbb20a2
shPark <guriguri1576@gmail.com>
2019-11-06 16:19:12 +0900

    Update geoserver_style.bat
    geoserver windows bat파일 설정한 db의 공간테이블 레이어 등록 하도록 스크립트 수정

--- windows/geoserver_style.bat
+++ windows/geoserver_style.bat
@@ -10,8 +10,11 @@ FOR /r %%f in (%1\*.sld) do (
 	echo %%~nf
 	echo %%~nf%%~xf
 	
+	# layer
+	curl -v -u %login% -XPOST -H "Content-type: text/xml" -d "<featureType><name>%%~nf</name></featureType>" %restapi%/workspaces/%workspace%/datastores/%store%/featuretypes?recalculate=nativebbox,latlonbbox
+	
 	rem style
-	echo -v -u %login% -XPOST -H "Content-type: text/xml" -d "<style><name>%%~nf</name><filename>%sldfile%</filename></style>" %restapi%/workspaces/%workspace%/styles
+	echo -v -u %login% -XPOST -H "Content-type: text/xml" -d "<style><name>%%~nf</name><filename>%%~nf%%~xf</filename></style>" %restapi%/workspaces/%workspace%/styles
 			
 	rem upload the SLD definition to the style
 	echo -v -u %login% -XPUT -H "Content-type: application/vnd.ogc.sld+xml" -d @%%~nf%%~xf %restapi%/workspaces/%workspace%/styles/%%~nf
